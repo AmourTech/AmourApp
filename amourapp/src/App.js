a7a05f4e17a6b545e8998923dd4931923cd43cbf
Aiden Brady <n10490698@qut.edu.au>
2021-08-04 11:17:36 +1000

    Added basic pages with routing

--- amourapp/src/App.js
+++ amourapp/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Clients from './Clients'
+import Proposals from './Proposals'
+import Services from './Services'
+import Library from './Library'
+import Settings from './Settings'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <aside>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/Clients'}>Clients</Link>
+          <Link to={'/Proposals'}>Proposals</Link>
+          <Link to={'/Services'}>Services</Link>
+          <Link to={'/Library'}>Library</Link>
+          <Link to={'/Settings'}>Settings</Link>
+        </aside>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/Clients" component={Clients} />
+          <Route path="/Proposals" component={Proposals} />
+          <Route path="/Services" component={Services} />
+          <Route path="/Library" component={Library} />
+          <Route path="/Settings" component={Settings} />
+        </main>
+      </div>
+    </Router>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
