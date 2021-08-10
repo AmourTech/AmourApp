266018b34fe41882fd281b74868380de5dd63ed6
Aiden Brady <n10490698@qut.edu.au>
2021-08-11 02:15:00 +1000

    initial design changes (bit messy)

--- amourapp/src/App.js
+++ amourapp/src/App.js
@@ -1,24 +1,55 @@
 import './App.css';
+import logo from './logo.svg';
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 import Home from './Home'
 import Clients from './Clients'
 import Proposals from './Proposals'
 import Services from './Services'
 import Library from './Library'
 import Settings from './Settings'
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+
+
 
 function App() {
+
   return (
+    <div className="App1">
+    <Navbar expand="lg">
+    <Navbar.Brand><img src={logo} className="App-logo" alt="logo" /></Navbar.Brand>
+      <Navbar.Brand>Username</Navbar.Brand>
+      <Form className="form-center">
+        <FormControl type="text" placeholder="Search" className="" />
+      </Form>
+      <Navbar.Collapse id="basic-navbar-nav">
+        {/* <Nav className="ml-auto">
+          <Nav.Item>Username</Nav.Item>
+        </Nav> */}
+      </Navbar.Collapse>
+    </Navbar>
+
     <Router>
       <div className="App">
         <aside>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/Clients'}>Clients</Link>
-          <Link to={'/Proposals'}>Proposals</Link>
-          <Link to={'/Services'}>Services</Link>
-          <Link to={'/Library'}>Library</Link>
-          <Link to={'/Settings'}>Settings</Link>
+          <ProSidebar>
+            <Menu>
+              <MenuItem><Link to={'/'}>Dashboard</Link></MenuItem>
+              <MenuItem><Link to={'/Clients'}>Clients</Link></MenuItem>
+              {/* <SubMenu title="Proposals">
+                <MenuItem><Link to={'/Proposals'}>Create a new proposal</Link></MenuItem>
+                <MenuItem>Export Proposals</MenuItem>
+                <MenuItem>Import Proposals</MenuItem>
+                <MenuItem>View Proposals</MenuItem>
+              </SubMenu> */}
+              <MenuItem><Link to={'/Proposals'}>Proposals</Link></MenuItem>
+              <MenuItem><Link to={'/Services'}>Services</Link></MenuItem>
+              <MenuItem><Link to={'/Library'}>Library</Link></MenuItem>
+              <MenuItem><Link to={'/Settings'}>Settings</Link></MenuItem>
+            </Menu>
+          </ProSidebar>
         </aside>
 
         <main>
@@ -31,24 +62,9 @@ function App() {
         </main>
       </div>
     </Router>
+    </div>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+
   );
 }
-
 export default App;
