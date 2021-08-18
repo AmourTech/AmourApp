9b22fcbf56c165542e1420371ede3d1c1d374cf4
Yaza George <yazagyi@live.com>
2021-08-18 12:44:41 +1000

    add employee base
    added placeholder states for add employee. still needs functionality and requires

--- amourapp/src/Settings.js
+++ amourapp/src/Settings.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import './App.css';
 import React from 'react'
 
@@ -9,7 +12,26 @@ function App() {
         <p>
           Settings
         </p>
+        <p>
+
+          Add Employee:
+        <InputGroup className="mb=3" >
+        <InputGroup.Text >First and Last Name</InputGroup.Text>
+        <FormControl id="Fname" aria-label="First name" placeholder="First Name"/>
+        <FormControl id="Lname" aria-label="Last name" placeholder="Last Name"/>
+        </InputGroup>
+        <InputGroup className="mb=3">
+        <InputGroup.Text>Email Address</InputGroup.Text> 
+        <FormControl id="Email" aira-label="Email" placeholder="Email Address"></FormControl>
+        </InputGroup>
+        <Button variant="dark"> add employee </Button>
+        </p>
+
+
       </header>
+
+
+
     </div>
   );
 }
