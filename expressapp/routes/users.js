e4f08d53a7e313e8f6a465a7ff050155ffa91fd1
Boyiiiii <137158557@qq.com>
2021-10-04 23:11:49 +0800

    Forget password function

--- expressapp/routes/users.js
+++ expressapp/routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const nodemailer = require('nodemailer');
 var app = express();
 
 
@@ -6,58 +7,6 @@ var app = express();
 
 var router = express.Router();
 var db = require( "../database/db.js" );
-
-var crypto = require('crypto');
-const nodemailer = require('nodemailer');
-
-
-router.post('/customerportal',function(req,res,next){
-
-	if (req.body.email == '') {
-		res.status(400).send('no email')
-	}
-	const token = crypto.randomBytes(20).toString('hex');
-	const expires = Date.now() + 3600000;
-	
-	db.query('UPDATE pro SET token = ?, expiry = ? where id=?', [token, expires, req.query.id],function (err, result) {
-		if(err){
-		 console.log('[INSERT ERROR] - ',err.message);
- res.send('0');
-		 return;
-		}
-	console.log(result)
-	res.send('1');
-	});
-
-	const transporter = nodemailer.createTransport({
-		service:'gmail',
-		auth: {
-			user: "emaileramapp@gmail.com",
-			pass: "amourapp1!",
-		},
-	});
-	const mailOptions = {
-		from: 'emaileramapp@gmail.com',
-		to: req.body.email,
-		subject: 'Link to Accept or Refuse proposal',
-		text:
-		"You are receiving this because you've been sent a proposal. \n\n"
-		+'Please click the following link, or past into your browser within one hour of receiving it: \n\n'
-		+'http://localhost:3001/viewproposal/' + token + '\n\n',
-	};
-	transporter.sendMail(mailOptions, (err, response) => {
-		if (err) {
-			console.error('there was an error\n' + req.body.email);
-		}
-		else {
-			console.log('res:', response);
-			res.status(200).json('email sent');
-		}
-	});
-})
-
-
-
 
 
 
@@ -79,91 +28,15 @@ router.get('/getservo',function(req,res,next){
 		
 });
 })
-router.get('/getterm',function(req,res,next){
-
-	db.query('SELECT * FROM terms WHERE Organisation = ?', [req.query.id],function (error, results, fields) {
-		if(error){
-			console.log('[INSERT ERROR] - ',error.message);
-			res.send('0');
-			return;
-		   }
-	
-		res.send(results)
-	
-});
-})
-router.post('/updateservo', function(req, res, next) {
+router.post('/applyservo', function(req, res, next) {
 	var data = req.body
 	
 	
 	
 	console.log(data)
-	db.query('UPDATE service SET DBill = ?, TaxRate = ?, XeroAccount=?, Spay =?, Cpay =?, Rpay =?, Sname = ?, SDesc = ? where ID=?', [data.Dbill,data.TRate,data.Xero,data.Spay,data.Cpay,data.Rpay,data.Sname,data.SDesc,data.id],function (err, result) {
+	db.query('UPDATE service SET DBill = ?, TaxRate = ?, XeroAccount=?, StandardPrice =? where Organisation=?', [data.Dbill,data.TRate,data.Xero,data.SPrice,data.org],function (err, result) {
 	        if(err){
 	         console.log('[INSERT ERROR] - ',err.message);
-			 res.send('0');
-	         return;
-	        }
-					
-		  console.log(result)
-	 
-	      res.send('1');
-	});
-	
-	
-});
-router.post('/updateterm', function(req, res, next) {
-	var data = req.body
-	
-	
-	
-	console.log(data)
-	db.query('UPDATE `terms` SET Terms = ?, TermName = ?, TermDesc = ? where TermID=?', [data.Terms,data.TName,data.TDesc,data.Tid],function (err, result) {
-	        if(err){
-	         console.log('[INSERT ERROR] - ',err.message);
-			 res.send('0');
-	         return;
-	        }
-					
-		  console.log(result)
-	 
-	      res.send('1');
-	});
-	
-	
-});
-router.post('/addservo', function(req, res, next) {
-	var data = req.body
-	
-	
-	
-	console.log(data)
-	var addSql ='INSERT INTO service(Organisation, DBill, TaxRate, XeroAccount, Spay, Cpay, Rpay, Sname, SDesc) VALUES (?,?,?,?,?,?,?,?,?)' ;
-	var addSqlParam = [data.org, data.Dbill,data.TRate,data.Xero,data.Spay,data.Cpay,data.Rpay,data.Sname,data.SDesc];
-	db.query(addSql, addSqlParam,function (err, result) {
-	        if(err){
-	         console.log('[INSERT ERROR] - ', err.message);
-			 res.send('0');
-	         return;
-	        }
-					
-		  console.log(result)
-	 
-	      res.send('1');
-	});
-	
-});
-router.post('/addterm', function(req, res, next) {
-	var data = req.body
-	
-	
-	
-	console.log(data)
-	var addSql ='INSERT INTO terms(Organisation, Terms, TermName,TermDesc) VALUES (?,?,?,?)' ;
-	var addSqlParam = [data.org, data.TName,data.TDesc,data.Terms];
-	db.query(addSql, addSqlParam,function (err, result) {
-	        if(err){
-	         console.log('[INSERT ERROR] - ', err.message);
 			 res.send('0');
 	         return;
 	        }
@@ -205,82 +78,6 @@ router.post('/add', function (req, res, next) {
 })
 
 
-
-
-router.post('/addprophelp', function (req, res, next) {
-	
-	var data = req.body
-	console.log(data.name)
-	var  addSql = 'INSERT INTO pro(ProposalID,ServiceID) VALUES ( req.ProposalID, req.ServiceID)';
-	var  addSqlParams = [data.name, data.client, data.sdate, data.edate, data.clen, data.message, data.acc, data.pay1, data.pay2, data.contact,data.userid];
-	db.query(addSql,addSqlParams,function (err, result) {
-			if(err){
-			 console.log('[INSERT ERROR] - ',err.message);
-			 res.send('0');
-			 return;
-			}        
-	 
-		  res.send('1');
-	});
-	
-	//res.send('Hello POST'+data);
- })
-
-router.post('/updatepro', function (req, res, next) {
-	
-	var data = req.body
-	console.log(data.name)
-	db.query("UPDATE pro SET name = ?, client = ?, sdate = ?, edate = ?, clen = ?, message = ?, acc = ?, pay1 = ?, pay2 = ?,contact = ? WHERE id = ?",[data.na, data.client, data.sda, data.eda, data.cle, data.mes, data.ac, data.py1, data.py2, data.cont, data.id],function (err, results) {
-					if(err){
-					 console.log('[INSERT ERROR] - ',err.message);
-		 res.send('0');
-					 return;
-					}        
-	 
-				res.send('1');
-				console.log(results)
-	});
-	
-	//res.send('Hello POST'+data);
-})
-
-router.post('/answerpro', function (req, res, next) {
-	
-	var data = req.body
-	console.log(data.name)
-	db.query("UPDATE pro SET acc = ? WHERE id = ?",[data.ac, data.id],function (err, results) {
-					if(err){
-					 console.log('[INSERT ERROR] - ',err.message);
-		 res.send('0');
-					 return;
-					}        
-	 
-				res.send('1');
-				console.log(results)
-	});
-	
-	//res.send('Hello POST'+data);
-})
-
-router.post('/updateclient', function (req, res, next) {
-	
-	var data = req.body
-	console.log(data.clientname)
-	db.query("UPDATE client SET clientname = ?, ABN = ?, ACN = ?, BAddress = ?, BName = ?, BSBAccountNumber = ?, BSBName = ?, Contact = ?, TFN = ?, Type = ? WHERE AccountID = ?",[data.clientname, data.abn, data.acn, data.baddress, data.bname, data.bsbaccountn, data.bsbname, data.contact, data.tfn, data.type, data.id],function (err, results) {
-					if(err){
-					 console.log('[INSERT ERROR] - ',err.message);
-		 res.send('0');
-					 return;
-					}        
-	 
-				res.send('1');
-				console.log(results)
-	});
-	
-	//res.send('Hello POST'+data);
-})
-
-
 router.get('/find', function(req, res, next) {
 	
 	//console.log(db)
@@ -291,8 +88,6 @@ router.get('/find', function(req, res, next) {
 	  res.send(results);
 	});
 });
-
-
 
 
 router.get('/findprop', function(req, res, next) {
@@ -326,27 +121,6 @@ router.get('/findus', function(req, res, next) {
 	});
 });
 
-router.get('/findcont', function(req, res, next) {
-	
-	//console.log(db)
-	db.query('SELECT email from contact WHERE organ = 13', [req.query.id],function (error, results, fields) {
-	  if (error) throw error;
-	  console.log('The solution is: ');
-	  res.send(results);
-	});
-});
-
-router.get('/viewproposal', function(req, res, next) {
-	
-	//console.log(token)
-	db.query('SELECT * from pro where token = ? AND expiry > ?', [req.query.token, Date.now()],function (error, results, fields) {
-	  if (error) throw error;
-	  //console.log('The solution is: ');
-	  res.send(results);
-	});
-});
-
-
 // router.get('/add', function(req, res, next) {
 	
 
@@ -372,15 +146,13 @@ router.get('/get', function(req, res, next) {
 router.get('/getcl', function(req, res, next) {
 	
 	console.log(db)
-	db.query('SELECT *  from client', function (error, results, fields) { //, contact WHERE organ = Handler AND (SELECT Organisation FROM user) = Handler
+	db.query('SELECT *  from client INNER JOIN contact ON Handler = organ', function (error, results, fields) {
 	  if (error) throw error;
 	  //console.log('The solution is: ');
 	  res.send(results);
 	});
 	
 });
-
-
 
 
 router.post('/acc', function(req, res, next) {
@@ -474,31 +246,8 @@ router.post('/addcl', function(req, res, next) {
 	
 	
 	console.log(data)
-	var  addSql = 'INSERT INTO client(clientname, Contact, AccountID,ABN,ACN,BAddress,BSBAccountnumber, BSBName, TFN, Type, Handler) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'; //need to set this last value to whatever your organisation id is in sql until we set up the system
-	var  addSqlParams = [data.clientname, data.contact, data.accountid, data.abn ,data.acn , data.baddress,data.bsbaccountn, data.bsbname, data.tfn, data.type, data.Handler];
-	db.query(addSql,addSqlParams,function (err, result) {
-	        if(err){
-	         console.log('[INSERT ERROR] - ',err.message);
-			 res.send('0');
-	         return;
-	        }
-					
-		  console.log(result)
-	 
-	      res.send('1');
-	});
-	
-});
-router.post('/addclient', function(req, res, next) {
-	
-	
-	var data = req.body
-	
-	
-	
-	console.log(data)
-	var  addSql = 'INSERT INTO client(clientname, Contact, AccountID,ABN,ACN,BAddress,BSBAccountnumber, BSBName, TFN, Type, Handler) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'; //need to set this last value to whatever your organisation id is in sql until we set up the system
-	var  addSqlParams = [data.clientname, data.Contact, data.AccountID, data.ABN ,data.ACN , data.BAddress,data.BSBAccountNumber, data.BSBName, data.TFN, data.Type, data.Handler];
+	var  addSql = 'INSERT INTO client(clientname, Contact, AccountID,ABN,ACN,BAddress,BSBAccountnumber, BSBName, TFN, Type, Handler) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)'; //need to set this last value to whatever your organisation id is in sql until we set up the system
+	var  addSqlParams = [data.clientname, data.contact, data.accountid, data.abn ,data.acn , data.baddress,data.bsbaccountn, data.bsbname, data.tfn, data.type];
 	db.query(addSql,addSqlParams,function (err, result) {
 	        if(err){
 	         console.log('[INSERT ERROR] - ',err.message);
@@ -524,28 +273,7 @@ router.get('/deuser', function(req, res, next) {
 	
  // res.send('respond with a resource');
 });
-router.get('/deservo', function(req, res, next) {
-	
-	console.log(req.query)
-	db.query('delete  from service where ID = ?', [req.query.id],function (error, results, fields) {
-	  if (error) console.log("oops");
-	  //console.log('The solution is: ');
-	  res.send('1');
-	});
-	
- // res.send('respond with a resource');
-});
-router.get('/determ', function(req, res, next) {
-	
-	console.log(req.query)
-	db.query('delete  from terms where ID = ?', [req.query.id],function (error, results, fields) {
-	  if (error) console.log("oops");
-	  //console.log('The solution is: ');
-	  res.send('1');
-	});
-	
- // res.send('respond with a resource');
-});
+
 router.get('/del', function(req, res, next) {
 	
 	
@@ -614,15 +342,118 @@ router.post('/login', function(req, res, next) {
 router.get('/udel', function(req, res, next) {
 	
 	console.log(req.query)
-	db.query('DELETE from client WHERE AccountID = ?', [req.query.id],function (error, results, fields) {
+	db.query('delete  from contact where ContactID = ?', [req.query.id],function (error, results, fields) {
 	  if (error) console.log("oops");
 	  //console.log('The solution is: ');
-	  res.send(fields);
-		console.log(fields);
+	  res.send('1');
 	});
+	
+ // res.send('respond with a resource');
+});
+
+router.get('/udelcl', function(req, res, next) {
+	
+	console.log(req.query)
+	db.query('delete  from client where AccountID = ?', [req.query.id],function (error, results, fields) {
+	  if (error) console.log("oops");
+	  //console.log('The solution is: ');
+	  res.send('1');
+	});
+	
  // res.send('respond with a resource');
 });
 
 
+//重置密码
+router.post('/respassword', function(req, res, next) {
+	
+	var data = req.body
+	var type = data.atype
+	
+	if(type == 1){
+		
+		db.query('UPDATE user SET  password = ? where email = ?', [data.rcpassword,data.remail],function (error, results, fields) {
+		  if (error){
+			  
+			  console.log("oops")
+			  res.send('0');
+			  return;
+		  } 
+		  
+		     res.send(results);
+		  
+		 
+		
+	})
+	
+	}else{
+		
+		db.query('UPDATE contact SET  password = ? where Email = ?', [data.rcpassword,data.remail],function (error, results, fields) {
+			console.log(results)
+			if(results.length == 0){
+				res.send('0');
+			}else{
+				res.send(results);
+			}
+		})
+	}
+	
+	  //console.log('The solution is: ');
+	
+	
+	
+ // res.send('respond with a resource');
+});
+
+router.get('/sendEmail', function(req, res, next) {
+	
+	
+	var num="";
+	for(var i=0;i<4;i++){
+		num+=Math.floor(Math.random()*10)
+	}
+
+	
+	 nodemailer.createTestAccount((err, account) => {
+		 
+
+	        let transporter = nodemailer.createTransport({
+	            host: 'smtp.163.com',
+				secure: true, // 
+	            port: 465,
+	            auth: {
+	                user: "wuhuairlinee@163.com", //
+	                pass: "FNMYOYSIXJWNVXLD" //
+	            }
+	        });
+	        let mailOptions = {
+	            //
+	            from: 'wuhuairlinee@163.com',
+	            to:  [req.query.email],
+	            subject: 'Verification code',
+	            text: '',
+	            html: '<b>Your verification code is:' + num + '</b>',
+	
+	        };
+	
+	        //
+	        transporter.sendMail(mailOptions, (error, info) => {
+				let fa = true;
+	            if (error) {
+	                return console.log(error);
+					     res.send('{"code":'+0+',"mes":'+num+'}');
+						 fa = false;
+					
+	            }
+				if(fa){
+					res.send('{"code":'+1+',"mes":'+num+'}');
+				}
+	        
+	        });
+	    });
+
+	 // res.send('1')
+   
+});
 
 module.exports = router;
